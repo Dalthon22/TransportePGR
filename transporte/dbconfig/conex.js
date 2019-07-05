b82d21095322e9f5344fb537b080411669ed6641
Dalton Diaz <carlos.diaz@strategi-k.com>
2019-07-04 23:31:27 -0600

    Archivo de conexion

--- transporte/dbconfig/conex.js
+++ transporte/dbconfig/conex.js
@@ -1,5 +1,24 @@
 const Sequelize = require('sequelize');
 
+//Azure DB connection Generic
+/*module.exports =  new Sequelize('sigestran', 'user','pssword', {
+  host: 'sigestran.database.windows.net',
+  dialect: 'mssql',
+  dialectOptions: {
+    options: {
+        encrypt: true,
+    }
+},
+
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  },
+});*/
+
+
 //Azure DB connection
 /*module.exports =  new Sequelize('sigestran', 'hq14006','Aomar2019', {
   host: 'sigestran.database.windows.net',
@@ -18,8 +37,43 @@ const Sequelize = require('sequelize');
   },
 });*/
 
+//Dalton: Azure DB connection
+/*module.exports =  new Sequelize('sigestran', 'dm15012','Cdalton2019', {
+  host: 'sigestran.database.windows.net',
+  dialect: 'mssql',
+  dialectOptions: {
+    options: {
+        encrypt: true,
+    }
+},
+
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  },
+});*/
+
 //Local db connection
-module.exports =  new Sequelize('procu', 'hq14006', 'hq14006', {
+/*module.exports = new Sequelize('procu', 'hq14006', 'hq14006', {
   host: 'localhost',
   dialect: 'mssql',
+});*/
+
+//Dalton: Local db connection
+const dbConex = new Sequelize('sigestran', 'DM15012', 'dm15012+', {
+  host: 'localhost',
+  dialect: 'mssql',
+  operatorAliases: false,
+
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  },
+
 });
+
+module.exports = dbConex;
