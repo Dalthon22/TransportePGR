4a4bad8dcd2b77d2eed653819b1291cdf1070e19
AxlHrndz <43728796+AxlHrndz@users.noreply.github.com>
2019-07-04 20:13:07 -0600

    semantic funcionando

--- transporte/app.js
+++ transporte/app.js
@@ -6,18 +6,18 @@ const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const nunjucks=require('nunjucks');
-const index = require('./routes/index');
+//const index = require('./routes/index');
 const Sequelize = require('sequelize');
 
 //Import Routes (Controllers)
-var indexRouter = require('./routes/index');
+/*var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var carRouter = require('./routes/c_car');
 
 //Usage of Routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/cars', carRouter);
+app.use('/cars', carRouter);*/
 
 
 var app = express();
@@ -32,11 +32,15 @@ db.authenticate()
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-/*nunjucks.configure('views', {
+app.set('view engine', 'nunjucks');
+nunjucks.configure('views', {
     autoescape: true,
     express: app
-});*/
+});
+//Renderiza la primera página de la aplicación al correr el servidor
+app.get('/', function(req, res) {
+  res.render('base.html');
+});
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -46,7 +50,6 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/scripts', express.static(`${__dirname}/node_modules/`));
-app.use('/', index);
 
 
 // catch 404 and forward to error handler
@@ -63,8 +66,12 @@ app.use(function(err, req, res, next) {
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  //res.status(err.status || 500);
+  //res.render('error');
 });
+
+//"localhost:3000" en barra de navegador
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
 
 module.exports = app;
