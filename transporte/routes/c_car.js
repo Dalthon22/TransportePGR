543eff8de076fc968854b8189a22ff0071282bd5
Dalton Diaz <carlos.diaz@strategi-k.com>
2019-07-05 23:35:28 -0600

    Vitsa funcionando

--- transporte/routes/c_car.js
+++ transporte/routes/c_car.js
@@ -4,13 +4,18 @@ const services = require('../services/s_car');
 const Car = require('../models/m_car');
 
 /*GET Car List*/
-router.get('/', (req, res) => {
+router.get('/', (req, res) =>
+    //cars = await services.getAll();
+    //.then(res.render('../views/Cars/list.html'), {
+    //  cars
+    //})
+    //.catch(errors => console.log(errors))
     services.getAll()
-        .then(cars => res.render('../views/Cars/list.html'), {
-            cars
-        })
-        .catch(errors => console.log(errors))
-});
+    .then(cars => res.render('../views/Cars/list.html', {
+        cars
+    }))
+    .catch(error => console.log(error))
+);
 
 /*Get Add a car */
 router.get('/add', (req, res) => {
