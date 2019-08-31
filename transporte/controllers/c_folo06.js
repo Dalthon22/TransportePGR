c95471143b06ee6a45350dac5976f430296b5876
AxlHrndz <43728796+AxlHrndz@users.noreply.github.com>
2019-08-31 15:31:11 -0600

    little update

--- transporte/controllers/c_folo06.js
+++ transporte/controllers/c_folo06.js
@@ -8,5 +8,26 @@ const { validationResult } = require('express-validator');
 class folo06_services {
     constructor(){}
 
+  //Gets Departments List
+  async getDepartmentList(req, res) {
+    try {
+      let Departamentos = await department_controller.getList();
+      return res.render('../views/folo06.html', {
+        Departamentos
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
+    //Gets Municipios list based on the selected department
+    async getMunicipiosByDepartamento(req, res) {
+        try {
+          let selectedDepartamento = req.query.selectedDepartamento;
+          let municipios = await municipio_controller.getMunicipios(selectedDepartamento);
+          res.send(municipios);
+        } catch (error) {
+          console.log(error);
+        }
+      };
 }
