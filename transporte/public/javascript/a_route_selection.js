2ef504828d8d7f3d5475f6bdb91b19724bee4052
ElmerHernandez <elmerh49@gmail.com>
2019-10-16 20:07:15 -0600

    route_selection update

--- transporte/public/javascript/a_route_selection.js
+++ transporte/public/javascript/a_route_selection.js
@@ -15,11 +15,43 @@ document.getElementById("btnInfoAceptar").addEventListener('click', function () 
 })
 
 
+/* para la seleccion de los checkbox */
+
+
+
+
+
+/* $("#selectall").on("click", function () {
+    $(".case").prop("checked", this.checked);
+});
+
+// if all checkbox are selected, check the selectall checkbox and viceversa  
+$(".case").on("click", function () {
+    if ($(".case").length == $(".case:checked").length) {
+        $("#selectall").prop("checked", true);
+    } else {
+        $("#selectall").prop("checked", false);
+    }
+}); */
+
+
+
+/* $(".micheckbox").on('change', function () {
+    if ($(this).is(':checked')) {
+        // Hacer algo si el checkbox ha sido seleccionado
+        alert("El checkbox con valor " + $(this).val() + " ha sido seleccionado");
+    } else {
+        // Hacer algo si el checkbox ha sido deseleccionado
+        alert("El checkbox con valor " + $(this).val() + " ha sido deseleccionado");
+    }
+}); */
+
+
 /* para utilizar data table */
 
-$(document).ready(function () {
+/* $(document).ready(function () {
     $('#selectroutes').DataTable({
-        /* "scrollY": "500px",
-        "scrollCollapse": true, */
+        // "scrollY": "500px",
+        //"scrollCollapse": true, 
     });
-});
+}); */
