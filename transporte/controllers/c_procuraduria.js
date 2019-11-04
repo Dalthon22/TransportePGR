fb2c7810098dbe5900c36d9c32edbcea7f0e2b23
AxlHrndz <43728796+AxlHrndz@users.noreply.github.com>
2019-11-03 20:26:45 -0600

    Update c_procuraduria.js

--- transporte/controllers/c_procuraduria.js
+++ transporte/controllers/c_procuraduria.js
@@ -6,7 +6,7 @@ const {
 } = require('express-validator');
 
 class Procuraduria_controller {
-  constructor() {}
+  constructor() { }
 
 
   async getGestionar(req, res) {
@@ -17,17 +17,29 @@ class Procuraduria_controller {
       let procu_id = req.query.procu_id;
       if (procu_id) {
         Procu = await Procuraduria.findByPk(procu_id);
-      }
-      let Departamentos = await Departamento.getList();
-      console.log(Departamentos);
-      return res.render('../views/procuraduria/add.html', {
-        Departamentos,
-        Procu
-      });
+        let Dir = await Address.findByPk(Procu.address_id);
+        let detail = Dir.detail;
+        let departamento = Dir.department_id;
+        let municipio = Dir.city_id;
+        let Departamentos = await Departamento.getList();
+        return res.render('../views/procuraduria/add.html', {
+          Procu,
+          detail,
+          departamento,
+          municipio,
+          Departamentos
+        });
+      } else {
+        Procu = await Procuraduria.findByPk(procu_id);
+        let Departamentos = await Departamento.getList();
+        return res.render('../views/procuraduria/add.html', {
+          Departamentos,
+          Procu
+        });
+      };
     } catch (error) {
       console.log(error);
     }
-
   };
 
   //Gets procuradurías list
@@ -81,20 +93,6 @@ class Procuraduria_controller {
     };
   };
 
-  //Renders edit form
-  async getUpdate(req, res) {
-    try {
-      let procu_id = req.query.procu_id;
-      console.log(procu_id);
-      let Procu = await Procuraduria.findByPk(procu_id);
-      return res.render('../views/procuraduria/add.html', {
-        Procu
-      });
-    } catch (error) {
-      console.log(error);
-    }
-  };
-
   //Saves the edited procuraduria in the DB
   async updateProcuraduria(req, res) {
     try {
@@ -102,17 +100,29 @@ class Procuraduria_controller {
       let {
         procu_id,
         name,
+        detail,
+        departamento,
+        municipio
       } = req.body;
+      let Procu = await Procuraduria.findByPk(procu_id);
       console.log(errors.array());
       if (!errors.isEmpty()) {
         //If there are errors, renders the same form, otherwise saves the edited Address
-        let Procu = await Procuraduria.findByPk(procu_id);
         res.render('../views/procuraduria/edit.html', {
           Procu,
           errors: errors.array()
         });
       } else {
         console.log(req.body);
+        Address.update({
+          detail,
+          city_id: municipio,
+          department_id: departamento
+        }, {
+          where: {
+            id: Procu.address_id
+          },
+        });
         Procuraduria.update({
           name: name
         }, {
@@ -121,7 +131,7 @@ class Procuraduria_controller {
           }
         });
         res.redirect('/instituciones');
-      }
+      };
     } catch (error) {
       console.log(error);
     }
