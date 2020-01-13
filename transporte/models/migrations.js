887a29a3a57634f80bdf4efc4a95005f8c6e985e
ElmerHernandez <elmerh49@gmail.com>
2020-01-12 21:47:30 -0600

    semi actualizando

--- transporte/models/migrations.js
+++ transporte/models/migrations.js
@@ -47,7 +47,7 @@ class Migration {
             alter: false
         });
         Voucher.sync({
-            alter: true
+            alter: false
         });
         Unit.sync({
             alter: false
@@ -86,7 +86,7 @@ class Migration {
             alter: false
         });
         Vehicle_folo6_assign.sync({
-            alter: true
+            alter: false
         });
         ProcurementBill.sync({
             alter: false
