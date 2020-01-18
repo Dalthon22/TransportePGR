a9968d128654cc59b6a4bb6dc72cad075ac06d17
Daniel Mejia <rdanielmejia7@gmail.com>
2020-01-18 02:23:49 -0600

    AUTHORIZE

--- transporte/app.js
+++ transporte/app.js
@@ -59,12 +59,12 @@ app.use('/solicitud_nueva', require('./routes/r_folo6'));
 app.use('/vales', authorize('adminV'), require('./routes/r_voucher'));
 app.use('/direccion', require('./routes/r_address'));
 app.use('/instituciones', authorize('adminIT'), require('./routes/r_procuraduria'));
-app.use('/misiones', require('./routes/r_misiones'));
+app.use('/misiones', authorize('adminTrans', 'adminR'), require('./routes/r_misiones'));
 app.use('/vehiculos', authorize('adminTrans'), require('./routes/r_vehicle'));
 
 app.use('/rutas', require('./routes/r_route'));
-app.use('/lugares_frecuentes', require('./routes/r_frequent_places'));
-app.use('/motoristas', require('./routes/r_driver'));
+app.use('/lugares_frecuentes', authorize('adminTrans', 'adminR'), require('./routes/r_frequent_places'));
+app.use('/motoristas', authorize('adminTrans'), require('./routes/r_driver'));
 app.use('/empleado', require('./routes/r_employee'));
 app.use('/panel_de_aprobacion', authorize('uBoss', 'adminTrans'), require('./routes/r_approve_panel'));
 app.use('/configuracion_calendario', require('./routes/r_config_calendar'));
