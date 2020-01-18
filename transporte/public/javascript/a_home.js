e40a62cfc97eaa57eb9ce974cce64fb98c72be54
Jorge Sibrian <50184999+JorgeSibrian98@users.noreply.github.com>
2020-01-17 22:43:24 -0600

    update home

--- transporte/public/javascript/a_home.js
+++ transporte/public/javascript/a_home.js
@@ -261,6 +261,7 @@ $('#mytable tbody').on('click', '.print.link.icon', function (event) {
             console.log("El folo se mostrará en seguida")
         }
     }).done(function (data, textStatus, jqXHR) {
+        console.log("imprimi")
         debugBase64(data.link);
         $('.segment').dimmer('hide');
     })
@@ -449,6 +450,7 @@ function noAnimateAddButton() {
 function debugBase64(base64URL) {
     var win = window.open();
     win.document.write('<iframe src="' + base64URL + '" frameborder="0" style="border:0; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%;" allowfullscreen></iframe>');
+    console.log(base64URL)
     win.document.close()
 }
 
